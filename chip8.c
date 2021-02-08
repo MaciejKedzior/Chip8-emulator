55fd5b2ea55f9e8caacd1917a342be4ede4538ea
MaciejKedzior <maciej.kedzior@student.uj.edu.pl>
2021-02-08 03:24:30 +0100

    Added goto, return and continue commands

--- chip8.c
+++ chip8.c
@@ -15,10 +15,10 @@ void initialize(struct chip8* chip8){
     chip8->sp = 0;
     chip8->opcode =0;
 
-    memset(chip8->gfx, 0, sizeof(unsigned char)*2048);
-    memset(chip8->stack, 0, sizeof(unsigned char)*16);
-    memset(chip8->key, 0, sizeof(unsigned char)*16);
-    memset(chip8->V, 0, sizeof(unsigned char)*16);
+    memset(chip8->gfx, 0, sizeof(uint16_t)*2048);
+    memset(chip8->stack, 0, sizeof(uint16_t)*16);
+    memset(chip8->key, 0, sizeof(uint16_t)*16);
+    memset(chip8->V, 0, sizeof(uint16_t)*16);
 
     for (int i=0; i < 80; i++){
         chip8->memory[i] = chip8_fontset[i];
@@ -34,8 +34,8 @@ void initialize(struct chip8* chip8){
 
 
 void loadApplication(struct chip8* chip8, const char* filename){
-    unsigned int open_code, read_code;
-    unsigned char buffer[4096];
+    uint32_t open_code, read_code;
+    uint16_t buffer[4096];
 
     open_code = open(filename, O_RDONLY);
     if (open_code == -1){
@@ -48,8 +48,13 @@ void loadApplication(struct chip8* chip8, const char* filename){
         exit(EXIT_FAILURE);
     }
 
-    for (int i=0; i < 4096; i++)
-        chip8->memory[i+512] = buffer[i];
+    if (read_code - 512 > 4096)
+        for (uint32_t i=0; i < 4096; i++)
+            chip8->memory[i+512] = buffer[i];
+    else{
+        printf("Error, the file is too long");
+        exit(EXIT_FAILURE);
+    }
 
 }
 
@@ -60,27 +65,46 @@ void emulateCycle(struct chip8* chip8){
     switch (chip8->opcode & 0xF000){
         case 0x0000:
             switch (chip8->opcode & 0x000F){
-            case 0x0000:        //0x00E0: Clears the screen
-                //execute code
+
+                case 0x0000:        //0x00E0: Clears the screen
+                    for (uint32_t i=0; i < 64*32; ++i) chip8->gfx[i] = 0x0;
+                    chip8->drawFlag = true;
+                    chip8->pc += 2;
                 break;
-            case 0x000E:        //0x00EE: Returns from the subroutine
-                //execute code
+
+                case 0x000E:        //0x00EE: Returns from the subroutine
+                    --chip8->sp;
+                    chip8->pc = chip8->stack[chip8->sp];
+                    chip8->pc += 2;
                 break;
-            default:
-                printf("Unknown opcode 0x%X\n", chip8->opcode);
+
+                default:
+                    printf("Unknown opcode 0x%X\n", chip8->opcode);
                 break;
             }
-            
-        case 0xA000:
-            chip8->I = chip8->opcode & 0x0FFF;
+        
+        case 0x1000:   // 0x1NNN - goto NNN - jump to address NNN
+            chip8->pc = chip8->opcode & 0x0FFF;
             chip8->pc += 2;
-            break;
+        break;
 
-        case 0x2000:
+        case 0x2000:    // 0x2NNN - *(0xNNN)() - calls subroutine at NNN
             chip8->stack[chip8->sp] = chip8->pc;
             ++chip8->sp;
             chip8->pc = chip8->opcode & 0x0FFF;
-            break;
+        break;
+
+        case 0x3000:    // 0x3XNN - if (VX == NN) - skips the next instruction if VX equal to NN
+            if (chip8->V[(chip8->opcode & 0x0F00) >> 8] == chip8->opcode & 0x00FF)
+                chip8->pc += 4;     //because we skip next instruction
+            else
+                chip8->pc += 2;
+        break;
+
+        case 0xA000:
+            chip8->I = chip8->opcode & 0x0FFF;
+            chip8->pc += 2;
+        break;
 
         case 0x0004:
             if (chip8->V[(chip8->opcode & 0x00F0) >> 4] > (0xFF - chip8->V[chip8->opcode & 0x0F00]))
@@ -89,9 +113,17 @@ void emulateCycle(struct chip8* chip8){
                 chip8->V[0xF] = 0;
             chip8->V[(chip8->opcode & 0x00F0) >> 8 ] += chip8->V[(chip8->opcode & 0x00F) >> 4];
             chip8->pc += 2;
-            break;
+        break;
+
+        case 0x0033:
+            chip8->memory[chip8->I] = chip8->V[(chip8->opcode & 0x0F00) >> 8] / 100;
+            chip8->memory[chip8->I+1] = (chip8->V[(chip8->opcode & 0x0F00) >> 8] / 10) % 10;
+            chip8->memory[chip8->I+2] = (chip8->V[(chip8->opcode & 0x0F00) >> 8] / 100) % 10;
+            chip8->pc += 2;
+        break;
 
         //more cases
+        
         default:
             printf("Unknown opcode 0x%X\n", chip8->opcode);
     }
--- chip8.h
+++ chip8.h
@@ -1,29 +1,30 @@
 #include <string.h>
 #include <stdbool.h>
+#include <inttypes.h>
 
 
 struct chip8{
     bool drawFlag;
 
-    unsigned short opcode;      //Current opcode
-    unsigned char memory[4096]; //Memory (size= 4KB)
+    uint8_t opcode;      //Current opcode
+    uint16_t memory[4096]; //Memory (size= 4KB)
 
-    unsigned char V[16];        //Registers (V0-VF)
-    unsigned short I;           //Index register
-    unsigned short pc;          //Program counter
+    uint16_t V[16];        //Registers (V0-VF)
+    uint8_t I;           //Index register
+    uint8_t pc;          //Program counter
 
-    unsigned char gfx[64*32];   //Total amount of pixels: 2048
+    uint16_t gfx[64*32];   //Total amount of pixels: 2048
 
-    unsigned char delay_timer;  //Delay timer
-    unsigned char sound_timer;  //Sound timer
+    uint16_t delay_timer;  //Delay timer
+    uint16_t sound_timer;  //Sound timer
 
-    unsigned short stack[16];   //Stack (16 levels)
-    unsigned short sp;          //Stack counter
+    uint8_t stack[16];   //Stack (16 levels)
+    uint8_t sp;          //Stack counter
 
-    unsigned char key[16];      //INPUT
+    uint16_t key[16];      //INPUT
 };
 
-unsigned char chip8_fontset[80] = { 
+uint16_t chip8_fontset[80] = { 
   0xF0, 0x90, 0x90, 0x90, 0xF0, // 0
   0x20, 0x60, 0x20, 0x20, 0x70, // 1
   0xF0, 0x10, 0xF0, 0x80, 0xF0, // 2
