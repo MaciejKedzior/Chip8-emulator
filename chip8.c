03a1f06d3dcb3624709ba85124373e8bb200c618
MaciejKedzior <maciej.kedzior@student.uj.edu.pl>
2021-02-13 17:48:54 +0100

    another round of opcodes

--- chip8.c
+++ chip8.c
@@ -107,7 +107,6 @@ void emulateCycle(struct chip8* chip8){
         
         case 0x1000:   // 0x1NNN - goto NNN - jump to address NNN
             chip8->pc = chip8->opcode & 0x0FFF;
-            chip8->pc += 2;
         break;
 
         case 0x2000:    // 0x2NNN - *(0xNNN)() - calls subroutine at NNN
@@ -151,32 +150,137 @@ void emulateCycle(struct chip8* chip8){
             switch (chip8->opcode & 0x000F){
                 case 0x0000:         // 0x8XY0 - Sets VX to the value of VY.
                     chip8->V[(chip8->opcode & 0x0F00) >> 8 ] = chip8->V[(chip8->opcode & 0x00F0) >> 4];
+                    chip8->pc += 2;
+                break;
+
+                case 0x0001:        // 0x8XY1 - Set VX to VX | VY
+                    chip8->V[(chip8->opcode & 0x0F00) >> 8] = chip8->V[(chip8->opcode & 0x0F00) >> 8] | chip8->V[(chip8->opcode & 0x00F0) >> 4];
+                    chip8->pc += 2;
+                break;
+                
+                case 0x0002:        // 0x8XY2 - Set VX to VX & VY
+                    chip8->V[(chip8->opcode & 0x0F00) >> 8] = chip8->V[(chip8->opcode & 0x0F00) >> 8] & chip8->V[(chip8->opcode & 0x00F0) >> 4];
+                    chip8->pc += 2;
+                break;
+
+                case 0x0003:        // 0x8XY3 - Set VX to VX ^ VY (XOR)
+                    chip8->V[(chip8->opcode & 0x0F00) >> 8] = chip8->V[(chip8->opcode & 0x0F00) >> 8] ^ chip8->V[(chip8->opcode & 0x00F0) >> 4];
+                    chip8->pc += 2;
+                break;
+
+                case 0x0004:        // 0x8XY4 - Adds VY to VX. VF is set to 1 when there's carry and 0 when there isn't
+                    if (chip8->V[(chip8->opcode & 0x00F0) >> 4] > (0xFF - chip8->V[chip8->opcode & 0x0F00]))
+                        chip8->V[0xF] = 1;      //Set to carry
+                    else
+                        chip8->V[0xF] = 0;
+                    chip8->V[(chip8->opcode & 0x00F0) >> 8 ] += chip8->V[(chip8->opcode & 0x00F) >> 4];
+                    chip8->pc += 2;
+                break;
+
+                case 0x0005:        // 0x8XY5 - VY is subtracted from VX. VF is set to 0 when there's a borrow, and 1 when there isn't. 
+                    if (chip8->V[(chip8->opcode & 0x00F0) >> 4] > (0xFF - chip8->V[chip8->opcode & 0x0F00]))
+                        chip8->V[0xF] = 1;      //Set to borrow
+                    else
+                        chip8->V[0xF] = 0;
+                    chip8->V[(chip8->opcode & 0x00F0) >> 8 ] += chip8->V[(chip8->opcode & 0x00F) >> 4];
+                    chip8->pc += 2;
+                break;
+
+                case 0x0006:        // 0x8XY6 - Stores the least significant bit of VX in VF and then shifts VX to the right by 1.
+                    chip8->V[0xF] = chip8->V[(chip8->opcode & 0x0F00) >> 8] & 0x1;
+                    chip8->V[(chip8->opcode & 0x0F00) >> 8] >>= 1;
+                    chip8->pc += 2;
+                break;
+
+                case 0x0007:        // 0x8XY7 - Sets VX to VY minus VX. VF is set to 0 when there's a borrow, and 1 when there isn't. 
+                    if (chip8->V[(chip8->opcode & 0x00F0) >> 4] > (0xFF - chip8->V[chip8->opcode & 0x0F00]))
+                        chip8->V[0xF] = 1;      //Set to borrow
+                    else
+                        chip8->V[0xF] = 0;
+                    chip8->V[(chip8->opcode & 0x00F0) >> 8 ] = chip8->V[(chip8->opcode & 0x00F) >> 4] - chip8->V[(chip8->opcode & 0x00F0) >> 8 ];
+                    chip8->pc += 2;
+                break;
+
+                case 0x000E:        // 0x8XYE - Stores the most significant bit of VX in VF and then shifts VX to the left by 1.
+                    chip8->V[0xF] = chip8->V[(chip8->opcode & 0x0F00) >> 8] >> 7;
+                    chip8->V[(chip8->opcode & 0x0F00) >> 8] <<=  1;
+                    chip8->pc += 2;
                 break;
             }
             
+        case 0x9000:    // 0x9XY0 - Skips the next instruction if VX doesn't equal VY. (Usually the next instruction is a jump to skip a code block) 
+            if (chip8->V[(chip8->opcode & 0x0F00) >> 8] != chip8->V[(chip8->opcode & 0x00F0) >> 4])
+                chip8->pc += 4;
+            else
+                chip8->pc += 2;
+        break;
 
-        case 0xA000:
+        case 0xA000:    // 0xANNN - Sets I to the address NNN. 
             chip8->I = chip8->opcode & 0x0FFF;
             chip8->pc += 2;
         break;
 
-        case 0x0004:
-            if (chip8->V[(chip8->opcode & 0x00F0) >> 4] > (0xFF - chip8->V[chip8->opcode & 0x0F00]))
-                chip8->V[0xF] = 1;      //Set to carry
-            else
-                chip8->V[0xF] = 0;
-            chip8->V[(chip8->opcode & 0x00F0) >> 8 ] += chip8->V[(chip8->opcode & 0x00F) >> 4];
+        case 0xB000:    // 0xBNNN - Jumps to the address NNN plus V0. 
+            chip8->pc = chip8->opcode & 0x0FFF + chip8->V[0x0];
+        break;
+
+
+        case 0xC000:   // 0xCXNN - Sets VX to the result of a bitwise and operation on a random number (Typically: 0 to 255) and NN. 
+            chip8->V[(chip8->opcode & 0x0F00) >> 8] = (chip8->opcode & 0x00FF) & rand();
             chip8->pc += 2;
         break;
 
-        case 0x0033:
+        //  NOW THERE WOULD BE FEW OPCODES CONNECTED WITH DISPLAY AND KEYS:
+        //  DXYN, EX9E, EXA1, FX0A 
+
+        case 0xF007:    // 0xFX07 - Sets VX to the value of the delay timer. 
+            chip8->V[(chip8->opcode & 0x0F00) >> 8] = chip8->delay_timer;
+            chip8->pc += 2;
+        break;
+
+        case 0xF015:    // 0xFX15 - Sets the delay timer to VX. 
+            chip8->delay_timer = chip8->V[(chip8->opcode & 0x0F00) >> 8];
+            chip8->pc += 2;
+        break;
+
+        case 0xF018:    // 0xFX18 - Sets the sound timer to VX. 
+            chip8->sound_timer = chip8->V[(chip8->opcode & 0x0F00) >> 8];
+            chip8->pc += 2;
+        break;
+
+        case 0xF01E:    // 0xFX1E - Adds VX to I. VF is not affected.
+            chip8->I += chip8->V[(chip8->opcode & 0x0F00) >> 8];
+            chip8->pc += 2;
+        break;
+
+        case 0xF033:    // 0xFX33 - Stores the binary-coded decimal representation of VX, with the most significant of three digits at the address in I, the middle digit at I plus 1, and the least significant digit at I plus 2. (In other words, take the decimal representation of VX, place the hundreds digit in memory at location in I, the tens digit at location I+1, and the ones digit at location I+2.) 
             chip8->memory[chip8->I] = chip8->V[(chip8->opcode & 0x0F00) >> 8] / 100;
             chip8->memory[chip8->I+1] = (chip8->V[(chip8->opcode & 0x0F00) >> 8] / 10) % 10;
             chip8->memory[chip8->I+2] = (chip8->V[(chip8->opcode & 0x0F00) >> 8] / 100) % 10;
             chip8->pc += 2;
         break;
 
-        //more cases
+        case 0xF029:    // 0xFX29 - Sets I to the location of the sprite for the character in VX. Characters 0-F (in hexadecimal) are represented by a 4x5 font. 
+            //to be added
+        break;
+
+        case 0xF055:    // 0xFX55 - Stores V0 to VX (including VX) in memory starting at address I. The offset from I is increased by 1 for each value written, but I itself is left unmodified
+            for (int i =0; i <= ((chip8->opcode & 0x0F00) >> 8); i++){
+                chip8->memory[chip8->I + i] = chip8->V[i];
+            }
+
+            chip8->I += ((chip8->opcode & 0x0F00) >> 8) + 1;
+					chip8->pc += 2;
+        break;
+
+        case 0xF065:    // 0xFX65 - Fills V0 to VX (including VX) with values from memory starting at address I. The offset from I is increased by 1 for each value written, but I itself is left unmodified.
+            for (int i =0; i <= ((chip8->opcode & 0x0F00) >> 8); i++){
+                chip8->V[i] = chip8->memory[chip8->I + i];
+            }
+
+            chip8->I += ((chip8->opcode & 0x0F00) >> 8) + 1;
+					chip8->pc += 2;
+        break;
 
         default:
             printf("Unknown opcode 0x%X\n", chip8->opcode);
